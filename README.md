613e19d8d1cd73090f8e6778f56b2574fd6af3de
Joe Scotto <contact@joescotto.com>
2022-07-13 19:26:58 -0400

    Working view pass

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # SwiftMail
+A package to handle opening email links in the default app.
 
-A description of this package.
+# Platforms
+Tested on iOS 15.4 but should work on iOS 13 and up.
+
+# Setup
+1. Add the package to your Swift Package Dependencies
+2. Whitelist the URL schemes in your `Info.plist`. **Please keep this in mind when updating the package.**
+```
+<key>LSApplicationQueriesSchemes</key>
+<array>
+    <string>googlegmail</string>
+    <string>ms-outlook</string>
+    <string>readdle-spark</string>
+    <string>ymail</string>
+    <string>airmail</string>
+</array>
+```
--- Sources/SwiftMail/SwiftMail.swift
+++ Sources/SwiftMail/SwiftMail.swift
@@ -1,6 +1,121 @@
-public struct SwiftMail {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+public struct SwiftMail<Content: View>: View {
+    @State private var showAlert = false
+    
+    private let label: Content
+    private let to: String
+    private let subject: String?
+    private let mailBody: String?
+    
+    // require info.plist passed in order to error check for schemes
+    private let info: Bundle
+    
+//    @State private var subject: String?
+//    @State private var emailBody: String?
+    
+//    public let alert = Alert(title: Text("Swift Mail"))
+    
+//    @Binding private var swiftMailAlert: Bool
+//
+//    enum URLSchemes: String, CaseIterable {
+//        case GMail = "googlegmail://",
+//             Outlook = "ms-outlook://",
+//             Yahoo = "ymail://",
+//             Spark = "readdle-spark://",
+//             AirMail = "airmail://",
+//             Default = "mailto:"
+//    }
+    
+    public init(to: String, subject: String? = nil, body: String? = nil, @ViewBuilder label: () -> Content) {
+        self.to = to
+        self.subject = subject
+        self.mailBody = body
+        self.label = label()
+        
+       
     }
+    
+    public var body: some View {
+        Button(action: {
+            showAlert = true
+            var nsDictionary: NSDictionary?
+        }) {
+            label
+        }
+        .actionSheet(isPresented: $showAlert) {
+                ActionSheet(title: Text("Multiple Email Apps"),
+                            message: Text("Choose an app to open"),
+                            buttons: [
+                                .cancel(),
+                                .destructive(
+                                    Text("Overwrite Current Workout")
+                                ),
+                                .default(
+                                    Text("Append to Current Workout")
+                                )
+                            ]
+                )
+            }
+//        .alert("Send email with", isPresented: $showAlert) {
+//            Button("Gmail", action: {})
+//            Button("Outlook", action: {})
+//            Button("Spark", role: .destructive, action: {})
+//            Button("Apple Mail", role: .cancel, action: {})
+//        }
+    }
+    
+    
+//    public var url: URL? {
+//        // Encode URLS
+//        let subjectEncoded = subject.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+//        let bodyEncoded = body.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+//
+//        let gmailUrl = URL(string: "googlegmail://co?to=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)")
+//        let outlookUrl = URL(string: "ms-outlook://compose?to=\(to)&subject=\(subjectEncoded)")
+//        let yahooMail = URL(string: "ymail://mail/compose?to=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)")
+//        let sparkUrl = URL(string: "readdle-spark://compose?recipient=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)")
+//        let defaultUrl = URL(string: "mailto:\(to)?subject=\(subjectEncoded)&body=\(bodyEncoded)")
+//
+//        if let gmailUrl = gmailUrl, UIApplication.shared.canOpenURL(gmailUrl) {
+//            return gmailUrl
+//        } else if let outlookUrl = outlookUrl, UIApplication.shared.canOpenURL(outlookUrl) {
+//            return outlookUrl
+//        } else if let yahooMail = yahooMail, UIApplication.shared.canOpenURL(yahooMail) {
+//            return yahooMail
+//        } else if let sparkUrl = sparkUrl, UIApplication.shared.canOpenURL(sparkUrl) {
+//            return sparkUrl
+//        }
+//
+//        return defaultUrl
+//    }
+////
+//    private func composeUrl() {
+//
+//    }
+//
+//    public func openUrl() {
+//        UIApplication.shared.open(URL(string: "https://youtube.com")!)
+//    }
+//
+//    public func getAvailableClients() -> [String] {
+//        var availableClients = [String]()
+//
+//        for scheme in URLSchemes.allCases {
+//            if let url = URL(string: scheme.rawValue), UIApplication.shared.canOpenURL(url) {
+//                availableClients.append(url.absoluteString)
+//            }
+//        }
+//
+//        swiftMailAlert = true
+//
+//        return availableClients
+//    }
+//
+//    public func test() {
+//
+//    }
 }
+
+
+// If multiple found, ask which to open in
